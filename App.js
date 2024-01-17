45d3db0f729f24527c23a7494ce5def14f647908
Gaston Avogadro <gaston.avogadro@gmail.com>
2024-01-17 19:53:27 -0300

    clase 1

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{paddingTop: 20, paddingHorizontal: 25}}>
+      <Text>Hola alumnos/as!!</Text>
+      <Text>Chau</Text>
       <StatusBar style="auto" />
     </View>
   );
