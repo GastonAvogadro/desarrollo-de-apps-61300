104f113a9010f3ed76959889630a67cdf47be115
Gaston Avogadro <gaston.avogadro@gmail.com>
2024-01-23 21:16:12 -0300

    clase 3

--- App.js
+++ App.js
@@ -1,25 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Usuario from './src/components/Usuario'
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import Usuario from "./src/components/Usuario";
+import cartLogo from "./assets/cart.png";
 
 export default function App() {
+  // useState y useEffect hooks para controlar el estado de la aplicación y el ciclo de vida de un componente
+
   return (
-    <View style={{paddingTop: 20, paddingHorizontal: 25}}>
-      <Text>Usuarios</Text>
-      <Usuario usuario="Fernando"/>
-      <Usuario usuario="Mariano"/>
-      <Usuario usuario="Gisel"/>
-      <Usuario usuario="Flavia"/>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: 8,
+          alignItems: "center",
+        }}
+      >
+        <Text>CARRITO</Text>
+        {/* <Image style={{width: 50, height: 50}} source={{uri: "https://t3.ftcdn.net/jpg/05/60/17/66/360_F_560176615_cUua21qgzxDiLiiyiVGYjUnLSGnVLIi6.jpg"}}/> */}
+
+        <Image style={{ width: 50, height: 50 }} source={cartLogo} />
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          style={{
+            borderColor: "gray",
+            borderWidth: 1,
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            borderRadius: 5,
+            width: "90%",
+          }}
+          placeholder="Ingrese un producto"
+        />
+        <TouchableOpacity>
+          <Text style={{ fontSize: 40 }}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.productList}>
+        <Text style={styles.product}>Remera</Text>
+        <Text style={styles.product}>Pantalón</Text>
+        <Text style={styles.product}>Gorra</Text>
+        <Text style={styles.product}>Remera</Text>
+        <Text style={styles.product}>Pantalón</Text>
+        <Text style={styles.product}>Gorra</Text>
+        <Text style={styles.product}>Remera</Text>
+        <Text style={styles.product}>Pantalón</Text>
+        <Text style={styles.product}>Gorra</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#ededed",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 14,
+  },
+  productList: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  product: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 4,
   },
 });
